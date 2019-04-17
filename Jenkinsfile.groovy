42b27024e7c3b73ad6ab1981db26a902fdd8959c
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-17 15:24:46 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 node("master"){
   stage ('Build') {
  
-    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/simple-java-maven-app.git'
+    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/webapp.git'
     withMaven(
         // Maven installation declared in the Jenkins "Global Tool Configuration"
       maven: 'M3'){
