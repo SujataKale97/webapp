8ca3a852f0a5dfd99290ba8c8e6fe585f3b3411e
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-17 16:37:47 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,7 +6,16 @@ node("master"){
         // Maven installation declared in the Jenkins "Global Tool Configuration"
       maven: 'M3'){
         
-    bat 'mvn -Dmaven.test.failure.ignore=true deploy'
+    bat 'mvn -Dmaven.test.failure.ignore=true clean package'
     }
+  }
+  
+  stage ('artifact')
+  {
+      archive 'target/*.war'
+  }
+  stage ('deploy')
+  {
+    bat '''copy C:\\Apps\\Jenkins\\jobs\\WebApp-Pipe\\workspace\\target\\*.war C:\\apache-tomcat-7.0.94\\webapps'''
   }
 }
