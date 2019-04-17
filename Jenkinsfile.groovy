4ff9d544eff0dcc660d963d07971536ad0ef45c6
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-17 15:14:56 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,19 @@
 pipeline {
   stage ('Build') {
  
-    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/simple-java-maven-app.git'
+    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/webapp.git'
     withMaven(
         // Maven installation declared in the Jenkins "Global Tool Configuration"
-      maven: 'M3'){
-        
-     bat label: '', script: '''javac HelloWorld.java
-java HelloWorld'''
+     maven: 'M3'){
+      
+      bat 'mvn package'
     }
+  }
+  
+  stage ('Deploy')
+  {
+     steps {
+                sh 'mvn -Dmaven.test.failure.ignore=true deploy' 
+            }
   }
 }
