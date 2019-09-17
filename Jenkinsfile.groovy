fa15e326da67de798ece95cc88fb692590f4c547
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-09-17 12:10:51 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,15 +4,14 @@ node("master"){
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/webapp.git'
     withMaven(
         // Maven installation declared in the Jenkins "Global Tool Configuration"
-      maven: 'M3'){
-        
+      maven: 'M3'){        
     bat 'mvn clean package'
     }
   }
   
   stage('SonarQube analysis') {
     withSonarQubeEnv('sonar') {
-     bat 'mvn clean package sonar:sonar -Dsonar.projectKey=Maven-WebApp -Dsonar.host.url=http://localhost:9000  '
+     bat 'mvn sonar:sonar -Dsonar.projectKey=Maven-WebApp -Dsonar.host.url=http://localhost:9000  '
     } 
   }
   
